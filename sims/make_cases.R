81e75a4897c1410c54fc3c0348113de577b548a9
colemonnahan <monnahc@uw.edu>
2014-12-17 13:20:21 -0800

    fixed small bug in cases, added E case since required

--- sims/make_cases.R
+++ sims/make_cases.R
@@ -13,16 +13,29 @@ Fcase <- c("years; c(1976:2013)",
            paste0("fvals; c(", paste(Fvals, collapse=","), ")"))
 writeLines(Fcase, paste0(case.dir, "F0-sna.txt"))
 
+## TODO: Update these to new package. Write data files:
 index100 <- c("fleets; c(1,2)", "sds_obs;list(.05,.05)",
-              "years;list(seq(1976,2013, by=4),seq(1976,2013, by=4))",
+              "years;list(seq(1978,2013, by=4),seq(1978,2013, by=4))",
             "cpar;c(1,1)")
 writeLines(index100, paste0(case.dir, "index100-sna.txt"))
-
 lcomp100 <- c("fleets; c(1,2)", "Nsamp; list(50,100)",
-              "years;list(seq(1976,2013, by=4),seq(1976,2013, by=4))",
-            "cpar;c(1,1)")
+              "years;list(seq(1978,2013, by=4),seq(1978,2013, by=4))",
+            "cpar;c(1,1)", "lengthbin_vector;NULL")
 writeLines(lcomp100, paste0(case.dir, "lcomp100-sna.txt"))
 agecomp100 <- c("fleets; c(1,2)", "Nsamp; list(50,100)",
-              "years;list(seq(1976,2013, by=4),seq(1976,2013, by=4))",
-            "cpar;c(1,1)")
+              "years;list(seq(1978,2013, by=4),seq(1978,2013, by=4))",
+            "cpar;c(1,1)", "agebin_vector;NULL")
 writeLines(agecomp100, paste0(case.dir, "agecomp100-sna.txt"))
+
+## DONT NEED THIS??
+## ## Write a base M case. TODO: shoudl be able to delete this in new package.
+## Mcase <- c("function_type; change_tv", "param; NatM_p_1_Fem_GP_1",
+##   "dev; rep(0, 38)")
+## writeLines(Mcase, paste0(case.dir, "M0-sna.txt"))
+
+Ecase <- c("natM_type; 1Parm ", "natM_n_breakpoints; NULL ", "natM_lorenzen; NULL ",
+  "natM_val; c(NA, NA) ",
+  "par_name; c('L_at_Amin', 'L_at_Amax', 'VonBert_K', 'CV_young', 'CV_old')",
+  "par_int; rep(NA, 5) ", "par_phase; rep(-1, 5) ", "forecast_num; 0 ",
+  "run_change_e_full; TRUE ")
+writeLines(Ecase, paste0(case.dir, "E0-sna.txt"))
